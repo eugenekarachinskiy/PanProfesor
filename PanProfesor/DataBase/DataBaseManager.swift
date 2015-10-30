9a196e91c651e2c716f189a77433fd5b77f05008
Eugene <eugenekarachinskiy@gmail.com>
2015-10-30 15:14:48 +0300

    Implement part of DataBaseManager

--- PanProfesor/DataBase/DataBaseManager.swift
+++ PanProfesor/DataBase/DataBaseManager.swift
@@ -10,22 +10,64 @@ import Foundation
 import CoreData
 
 class DataBaseManager {
+    static let kDataBaseName = "PanProfesor"
     
-    var managedObjectModel: NSManagedObjectModel? {
-        let url = NSBundle.mainBundle().URLForResource("DataBaseModel", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: url)
+    class var defaultManager: DataBaseManager {
+        struct Static {
+            static let instance = DataBaseManager()
+        }
+        
+        return Static.instance
     }
     
-    var persistentStoreCoordinator: NSPersistentStoreCoordinator? {
-        return NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel!)
+    lazy var applicationDocumentDirectory: NSURL? = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls.first
+    }()
+    
+    lazy var managedObjectModel: NSManagedObjectModel? = {
+        guard let modelURL = NSBundle.mainBundle().URLForResource(DataBaseManager.kDataBaseName, withExtension: "momd") else {
+            return nil
+        }
+        
+        return NSManagedObjectModel(contentsOfURL: modelURL)
+    }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        guard let objectModel = self.managedObjectModel else {
+            return nil
+        }
+
+        let url = (self.applicationDocumentDirectory?.URLByAppendingPathComponent(DataBaseManager.kDataBaseName + ".sqlite"))
+        var coordinator: NSPersistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: objectModel)
+        
+        do {
+           try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+        } catch let error {
+            print("error persistentStoreCoordinator \(error)")
+        }
+        
+        return coordinator
+    }()
+    
+    lazy var manangedObjectContext: NSManagedObjectContext? = {
+       let context = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.MainQueueConcurrencyType)
+        context.persistentStoreCoordinator = self.persistentStoreCoordinator
+        return context
+    }()
+    
+    
+    func saveContext() {
+        guard let context = self.manangedObjectContext else {
+            return
+        }
+        
+        context.persistentStoreCoordinator?.performBlockAndWait({ () -> Void in
+            do {
+                try context.save()
+            } catch let error {
+                print("save context error \(error)")
+            }
+        })
     }
-    
-    
-    func applicationDocumentDirectory() -> NSURL? {
-        let urls = NSFileManager.defaultManager().URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask)
-        return urls.last
-    }
-    
-    
-    
 }
---
+++ PanProfesor/View Controllers/ChooseTranslateViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChooseTranslateViewController.swift
+//  PanProfesor
+//
+//  Created by Eugene Karachinskiy on 10/30/15.
+//  Copyright © 2015 Eugene Karachinskiy. All rights reserved.
+//
+
+import UIKit
+
+class ChooseTranslateViewController: BaseViewController {
+    
+//    @IBOutlet weak var collectionView: UICollectionView!
+//    
+//    
+//    let records: [Record] = [Record(text: "Волосы", translateText: "włosy"), Record(text: "Кожа", translateText: "skóra"), Record(text: "лоб", translateText: "czoło"), Record(text: "висок", translateText: "skroń"), Record(text: "бровь", translateText: "brew")]
+//
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+    
+}
